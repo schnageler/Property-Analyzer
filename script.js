14d2bb294668ca1ec9c8bb2efe7ada1a89531e0c
schnageler <83382965+schnageler@users.noreply.github.com>
2023-04-25 08:07:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ document.getElementById('propertyForm').addEventListener('submit', async (event)
     const todayProperties = await readCSV(todayInput.files[0]);
     const yesterdayProperties = await readCSV(yesterdayInput.files[0]);
 
-    // Perform your analysis here
     const result = analyzeProperties(todayProperties, yesterdayProperties);
-
     displayResult(result);
 });
 
@@ -23,7 +21,7 @@ async function readCSV(file) {
         const property = {};
 
         headers.forEach((header, index) => {
-            property[header] = values[index];
+            property[header.trim()] = values[index].trim();
         });
 
         return property;
@@ -33,32 +31,59 @@ async function readCSV(file) {
 }
 
 function analyzeProperties(todayProperties, yesterdayProperties) {
-    // Your analysis logic goes here
     const result = {};
 
-    // Example: Count properties in each list
-    result.today_count = todayProperties.length;
-    result.yesterday_count = yesterdayProperties.length;
+    const todayUnitIDs = new Set(todayProperties.map(property => property['Unit ID']));
+    const yesterdayUnitIDs = new Set(yesterdayProperties.map(property => property['Unit ID']));
+
+    // Count unique properties in each list
+    result.today_count = todayUnitIDs.size;
+    result.yesterday_count = yesterdayUnitIDs.size;
+
+    // Find new properties and removed properties
+    const newProperties = [...todayUnitIDs].filter(id => !yesterdayUnitIDs.has(id));
+    const removedProperties = [...yesterdayUnitIDs].filter(id => !todayUnitIDs.has(id));
+
+    result.new_properties_count = newProperties.length;
+    result.removed_properties_count = removedProperties.length;
+    result.new_properties = newProperties;
+    result.removed_properties = removedProperties;
 
     return result;
 }
-
 
 function displayResult(result) {
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = ''; // Clear previous results
 
-    // Display the result, e.g., as a table or list
     const table = document.createElement('table');
+
     for (const key in result) {
-        const row = document.createElement('tr');
-        const keyCell = document.createElement('td');
-        keyCell.textContent = key;
-        row.appendChild(keyCell);
-        const valueCell = document.createElement('td');
-        valueCell.textContent = result[key];
-        row.appendChild(valueCell);
-        table.appendChild(row);
+        if (key === 'new_properties' || key === 'removed_properties') {
+            const rowHeader = document.createElement('tr');
+            const headerCell = document.createElement('th');
+            headerCell.textContent = key === 'new_properties' ? 'New Properties' : 'Removed Properties';
+            rowHeader.appendChild(headerCell);
+            table.appendChild(rowHeader);
+
+            result[key].forEach(unitID => {
+                const row = document.createElement('tr');
+                const valueCell = document.createElement('td');
+                valueCell.textContent = unitID;
+                row.appendChild(valueCell);
+                table.appendChild(row);
+            });
+        } else {
+            const row = document.createElement('tr');
+            const keyCell = document.createElement('td');
+            keyCell.textContent = key;
+            row.appendChild(keyCell);
+            const valueCell = document.createElement('td');
+            valueCell.textContent = result[key];
+            row.appendChild(valueCell);
+            table.appendChild(row);
+        }
     }
+
     resultDiv.appendChild(table);
 }
